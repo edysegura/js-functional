05f4a0f6655845449506f8194f65a3b6444e2c05
Edy Segura <ednardo@inatel.br>
2018-02-27 00:22:31 -0300

    Added flatMap example

--- monad/index.js
+++ monad/index.js
@@ -1,10 +1,38 @@
+const fetch = require('node-fetch')
 const Bacon = require('baconjs')
-const stream = new Bacon.Bus()
 
+function getInPortuguese(word) {
+  const apiKey = ''
+
+  const endpoit =
+    "https://www.googleapis.com" +
+      "/language/translate/v2" +
+      "?key=" + apiKey +
+      "&source=en" +
+      "&target=pt" +
+      "&q=" + encodeURIComponent(word)
+
+    const promise = fetch(endpoit)
+      .then(response => response.json())
+      .then(
+        responseJson =>
+          responseJson
+            .data
+            .translations[0]
+            .translatedText
+      )
+      .catch(error => console.log(error))
+
+  const stream = Bacon.fromPromise(promise)
+  return stream
+}
+
+const stream = new Bacon.Bus()
 stream
+  .flatMap(word => getInPortuguese(word))
   .map(word => word.toUpperCase())
   .onValue(word => console.log(word))
 
 stream.push('cat')
 stream.push('meal')
-stream.push('trumpet')
+stream.push('cow')
