968088b5e003e88f01e574dd439a59b3fe6816bd
Edy Segura <ednardo@inatel.br>
2018-03-05 23:01:41 -0300

    Refactoring filter, map and sort example

--- filter-map-sort/index.js
+++ filter-map-sort/index.js
@@ -1,26 +1,26 @@
 "use strict"
 
 const collection = [
-  {name:"Lidy", gender:"female"},
-  {name:"", gender:"unknown"},
-  {name:"Guto", gender:"male"},
-  {name:"Daniel", gender:"male"},
-  {name:"", gender:"unknown"},
-  {name:"Edy", gender:"male"},
-  {name:"Udit", gender:"male"},
-  {name:"Scott", gender:"male"},
-  {name:"", gender:"unknown"},
-  {name:"", gender:"unknown"},
-  {name:"Colin", gender:"male"},
-  {name:"Luciano", gender:"male"}
-]
+  { name: 'Lidy', gender: 'female' },
+  { name: '', gender: 'unknown' },
+  { name: 'Guto', gender: 'male' },
+  { name: 'Daniel', gender: 'male' },
+  { name: '', gender: 'unknown' },
+  { name: 'Edy', gender: 'male' },
+  { name: 'Udit', gender: 'male' },
+  { name: 'Scott', gender: 'male' },
+  { name: '', gender: 'unknown' },
+  { name: '', gender: 'unknown' },
+  { name: 'Colin', gender: 'male' },
+  { name: 'Luciano', gender: 'male' }
+];
 
 const hasName = item => !!item.name //it outputs a boolean
 const onlyName = item => item.name
 
 const orderedNames = collection
-                    .filter(hasName)
-                    .map(onlyName)
-                    .sort()
+                      .filter(hasName)
+                      .map(onlyName)
+                      .sort()
 
 console.log(orderedNames)
