9114f5e01aaf83e8171057837f449d338e0d15da
Edy Segura <edysegura@gmail.com>
2018-08-14 15:56:58 -0300

    Updated function composition

--- function-composition/index.js
+++ function-composition/index.js
@@ -1,10 +1,10 @@
 const adder  = param => param + 1
-const addtip = param => param * 1.15
-const taxes  = param => param * 1.13
+const addTip = param => param * 1.15
+const addTax = param => param * 1.13
 
 const compose = (...functions) => (...params) => {
   return functions.map(pipe => pipe(params))
 }
-const composedFunction = compose(adder, addtip, taxes)
+const composedFunction = compose(adder, addTip, addTax)
 
 console.log(composedFunction('37'))
