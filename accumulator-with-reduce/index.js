3ab343cbbfab1588aac1fb83519c1a20d88f0c72
Edy Segura <edysegura@gmail.com>
2019-01-30 22:54:20 -0200

    Add 'use strict' to accumulator example

--- accumulator-with-reduce/index.js
+++ accumulator-with-reduce/index.js
@@ -1,16 +1,20 @@
+'use strict'
+
 const footballPlayers = [
-    { gender: "M", name: "Karim Benzema", age: 29, club: "Real Madrid CF", country: "Spain" },
-    { gender: "M", name: "Samir Nasri", age: 29, club: "Sevilla FC", country: "Spain" },
-    { gender: "M", name: "Hatem Ben Arfa", age: 29, club: "PSG", country: "France" },
-    { gender: "M", name: "Radamel Falcao", age: 27, club: "AS Monaco FC", country: "France" },
-    { gender: "M", name: "Riyad Mahrez", age: 25, club: "Leicester City FC", country: "England" },
-    { gender: "M", name: "Sofiane Feghouli", age: 26, club: "West Ham United", country: "England" }
+  { gender: "M", name: "Karim Benzema", age: 29, club: "Real Madrid CF", country: "Spain" },
+  { gender: "M", name: "Samir Nasri", age: 29, club: "Sevilla FC", country: "Spain" },
+  { gender: "M", name: "Hatem Ben Arfa", age: 29, club: "PSG", country: "France" },
+  { gender: "M", name: "Radamel Falcao", age: 27, club: "AS Monaco FC", country: "France" },
+  { gender: "M", name: "Riyad Mahrez", age: 25, club: "Leicester City FC", country: "England" },
+  { gender: "M", name: "Sofiane Feghouli", age: 26, club: "West Ham United", country: "England" }
 ]
 
 const groupByCountry = (players, player) => {
-    players[player.country.toLowerCase()].push(player)
-    return players
+  players[ player.country.toLowerCase() ].push(player)
+  return players
 }
 
-const playerByCountry = footballPlayers.reduce(groupByCountry, { france: [], england: [], spain: [] })
+const playerByCountry = footballPlayers
+  .reduce(groupByCountry, { france: [], england: [], spain: [] })
+
 console.log(playerByCountry)
