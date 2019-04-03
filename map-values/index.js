8249670a5555875f7b3c459e54a14ac329cbeb13
Edy Segura <edysegura@gmail.com>
2019-04-02 23:14:15 -0300

    Refactor map-values example

--- map-values/index.js
+++ map-values/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const convertToFahrenheit = t => t * 1.8 + 32
+const convertToFahrenheit = celsiusDegrees => celsiusDegrees * 1.8 + 32
 
 const celsius = [-15, -5, 0, 10, 16, 20, 24, 32]
 const fahrenheit = celsius.map(convertToFahrenheit)
