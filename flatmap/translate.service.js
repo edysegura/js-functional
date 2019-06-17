4f48affa4944601efbd654c3a0a6fde85576081a
Edy Segura <edysegura@gmail.com>
2019-06-17 15:43:49 -0300

    Change translate.service.js indentation

--- flatmap/translate.service.js
+++ flatmap/translate.service.js
@@ -5,12 +5,13 @@ function buildEndpoint(word) {
   const apiKey = process.env.API_KEY
 
   const endpoint =
-    "https://www.googleapis.com" +
-      "/language/translate/v2" +
-      "?key=" + apiKey +
-      "&source=en" +
-      "&target=pt" +
-      "&q=" + encodeURIComponent(word)
+    'https://www.googleapis.com' +
+    '/language/translate/v2' +
+    '?key=' + apiKey +
+    '&source=en' +
+    '&target=pt' +
+    '&q=' +
+    encodeURIComponent(word)
 
   return endpoint
 }
@@ -20,18 +21,12 @@ function getInPortuguese(word) {
 
   const promise = fetch(endpoint)
     .then(response => response.json())
-    .then(
-      responseJson =>
-        responseJson
-          .data
-          .translations[0]
-          .translatedText
-    )
+    .then(responseJson => responseJson.data.translations[0].translatedText)
     .catch(error => console.log(error))
 
   return Bacon.fromPromise(promise)
 }
 
 module.exports = {
-    getInPortuguese
+  getInPortuguese
 }
