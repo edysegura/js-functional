10377290623120bd38f750f41297dab8800a28a9
Edy Segura <ednardo.segura@mundiale.com.br>
2021-04-16 21:33:46 -0300

    Refactor sum with reduce

--- sum-with-reduce/index.js
+++ sum-with-reduce/index.js
@@ -1,16 +1,14 @@
 "use strict"
 
 const items = [
-  {name: 'New York Steak', price: 30.50},
-  {name: 'Bone shake', price: 13.50},
-  {name: 'Coffee', price: 3.22}
+  { name: 'New York Steak', price: 30.50 },
+  { name: 'Bone shake', price: 13.50 },
+  { name: 'Coffee', price: 3.22 }
 ]
 
-const adder = (previousValue, currentValue) => previousValue + currentValue
 const onlyPrice = item => item.price
+const adder = (previousValue, currentValue) => previousValue + currentValue
 
-const total = items
-              .map(onlyPrice)
-              .reduce(adder)
+const total = items.map(onlyPrice).reduce(adder)
 
 console.log('Total: ' + total)
