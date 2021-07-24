23d45331caaa75fa369ef369ca318b5835033591
Edy Segura <edysegura@gmail.com>
2021-07-23 22:46:14 -0300

    refactor: map values

--- map-values/index.js
+++ map-values/index.js
@@ -1,9 +1,10 @@
 const convertToFahrenheit = celsiusDegrees => celsiusDegrees * 1.8 + 32
-const formatTemperature = temperature => `${temperature.toFixed(2)} °F`
+const numberFormat = temperature => `🔥 ${temperature.toFixed(2)} °F`
 
 const celsius = [-15, -5, 0, 10, 16, 20, 24, 32]
+
 const fahrenheit = celsius
   .map(convertToFahrenheit)
-  .map(formatTemperature)
+  .map(numberFormat)
 
 console.table(fahrenheit)
