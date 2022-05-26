329081aacf764aedb90c18b36fc2f665fd3e3069
Edy Segura <edysegura@gmail.com>
2022-05-25 22:28:51 -0300

    refactor: lookup table

--- lookup-table/index.js
+++ lookup-table/index.js
@@ -2,6 +2,7 @@ const users = [
   { id: 11, name: 'Adam', age: 23, group: 'editor' },
   { id: 47, name: 'John', age: 28, group: 'admin' },
   { id: 85, name: 'William', age: 34, group: 'editor' },
+  { id: 39, name: 'Edy Segura', age: 34, group: 'admin' },
   { id: 97, name: 'Oliver', age: 28, group: 'admin' }
 ]
 
@@ -13,4 +14,4 @@ const indexingById = (accumulator, it) => {
 const lookupTable = users.reduce(indexingById, {})
 
 console.log(lookupTable)
-console.log(lookupTable[11].name)
+console.log('Name for id 39: ', lookupTable[39].name)
